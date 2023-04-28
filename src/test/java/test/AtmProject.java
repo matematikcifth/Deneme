d30000179cfb8365f67935b1e6abf125132dbc8f
Fatih <matematikcifth@gmail.com>
2023-04-28 22:16:27 +0200

    v4

--- src/test/java/test/AtmProject.java
+++ src/test/java/test/AtmProject.java
@@ -8,6 +8,8 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java Earn Money");
         //versiyon3
+        System.out.println("Git ogrenmek cok kolay");
+        //version4
 
     }
 
